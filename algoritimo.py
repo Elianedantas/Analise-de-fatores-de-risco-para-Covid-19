c494db4bff331ffe96745c613a12b5f179e4f171
Eliane Dantas <edantas241@gmail.com>
2023-10-05 17:01:30 -0300

    Correção de bugs e teste com base menor

--- algoritimo.py
+++ algoritimo.py
@@ -4,8 +4,8 @@ from sklearn import preprocessing
 from sklearn.linear_model import Perceptron
 
 #importando a base de dados atraves do gitHub
-url = 'https://raw.githubusercontent.com/Elianedantas/Analise-de-fatores-de-risco-para-Covid-19/main/Registros_Casos_e_obitos_ESP.csv'
-base_Treinamento = pd.read_csv(url,sep=',', encoding = 'latin1').values
+url = 'https://raw.githubusercontent.com/Elianedantas/RedeNeuralPerceptronDiabetes/main/Registros_Casos_e_obitos_ESP.csv'
+base_Treinamento = pd.read_csv(url,sep=',', encoding = 'utf-8').values
 
 #normalizando os dados
 def normalizarDados(base_Treinamento):
@@ -14,7 +14,7 @@ def normalizarDados(base_Treinamento):
 
     #Transforma valores categóricos equidistantes em valores binários equidistantes.
     #Atributos categóricos com valores sim e não
-    lb.fit(['SIM', 'IGNORADO', 'NÃO'])
+    lb.fit(['SIM', 'NÃO'])
     Asma = lb.transform(base_Treinamento[:,0])
     Cardiopatia = lb.transform(base_Treinamento[:,1])
     Diabetes = lb.transform(base_Treinamento[:,3])
@@ -40,12 +40,12 @@ def treinarRedePerceptron(atributos_norm, diagnostico_norm):
 
 def Treinar(Asma,Cardiopatia,Diabetes,Doenca_Hepatica,Doenca_Neurologica,Doenca_Renal,Idade,Obesidade,Pneumopatia,Síndrome_De_Down):
     dadosT = normalizarDados(base_Treinamento)
-    diagnostico_norm = base_Treinamento[:, 14]
+    diagnostico_norm = list(map(int, base_Treinamento[:, 14]))
 
     modelo = treinarRedePerceptron(dadosT, diagnostico_norm)
 
     teste = [[Asma, Cardiopatia, Diabetes, Doenca_Hepatica, Doenca_Neurologica, Doenca_Renal, Idade, Obesidade, Pneumopatia, Síndrome_De_Down]]
 
     min_max = preprocessing.MinMaxScaler()
-    testeT = min_max.transform(teste)
+    testeT = min_max.fit_transform(teste)
     return modelo.predict(testeT)
