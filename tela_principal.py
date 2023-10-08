e7139342c62b0542cc656b6e7411c5aed833592a
Natan Vinícius <88415998+NatanVini7@users.noreply.github.com>
2023-10-08 19:08:29 -0300

    Update tela_principal.py

--- tela_principal.py
+++ tela_principal.py
@@ -6,7 +6,7 @@ def diagnosticar():
 
     if a.Treinar(radio_Asma.get(), radio_Cardiopatia.get(), radio_Diabetes.get(), radio_Doenca_Hepatica.get(), 
         radio_Doenca_Neurologica.get(), radio_Doenca_Renal.get(), box_Idade.get(), radio_Obesidade.get(), 
-        radio_Pneumopatia.get(), radio_Síndrome_De_Down.get()) == 1:
+        radio_Pneumopatia.get(), radio_Doenca_Hematologica.get()) == 1:
         messagebox.showwarning(title="Resultado do diagnóstico",
         message='Você possui maiores fatores de risco para a covid-19.')     
     else:
@@ -18,20 +18,20 @@ janela = Tk()
 radio_Asma = IntVar()
 radio_Cardiopatia = IntVar()
 radio_Diabetes = IntVar()
+radio_Doenca_Hematologica = IntVar()
 radio_Doenca_Hepatica = IntVar()
 radio_Doenca_Neurologica = IntVar()
 radio_Doenca_Renal = IntVar()
 box_Idade = IntVar()
 radio_Obesidade = IntVar()
 radio_Pneumopatia = IntVar()
-radio_Síndrome_De_Down = IntVar()
 
 janela.title("Fatores de risco para Covid-19")
 largura = 800
 altura = 600
-pos_x = (janela.winfo_screenwidth() // 2) - (largura // 2)  # Centraliza horizontalmente
-pos_y = (janela.winfo_screenheight() // 2) - (altura // 2)  # Centraliza verticalmente
-janela.geometry(f"{largura}x{altura}+{pos_x}+{pos_y}")  # Defina a geometria da janela
+pos_x = (janela.winfo_screenwidth() // 2) - (largura // 2) 
+pos_y = (janela.winfo_screenheight() // 2) - (altura // 2) 
+janela.geometry(f"{largura}x{altura}+{pos_x}+{pos_y}") 
 
 def on_canvas_configure(event):
     canvas.configure(scrollregion=canvas.bbox("all"))
@@ -58,18 +58,18 @@ titulo1 = Label(frame_interno,
                 relief="groove",
                 border=1
                 )
-titulo1.grid(column=0, row=0, columnspan=2, pady=(0, 17), padx=20, sticky="w")  # Alinhe à esquerda
+titulo1.grid(column=0, row=0, columnspan=2, pady=(0, 17), padx=20, sticky="w") 
 
 perguntas_respostas = [
     ("Possui asma?", radio_Asma),
     ("Possui cardiopatia?", radio_Cardiopatia),
     ("Possui diabetes?", radio_Diabetes),
+    ("Possui doença hematologica?", radio_Doenca_Hematologica),
     ("Possui doença hepática?", radio_Doenca_Hepatica),
     ("Possui doença neurológica?", radio_Doenca_Neurologica),
     ("Possui doença renal?", radio_Doenca_Renal),
     ("Possui obesidade?", radio_Obesidade),
     ("Possui pneumopatia?", radio_Pneumopatia),
-    ("Possui Síndrome de Down?", radio_Síndrome_De_Down),
     ("Idade:", box_Idade)
 ]
 
@@ -111,13 +111,13 @@ botaoDiagnostico = Button(frame_interno,
                           relief="ridge",
                           border=2,
                           )
-botaoDiagnostico.grid(column=0, row=linha+3, columnspan=2, pady=20, padx=20)
+botaoDiagnostico.grid(column=0, row=linha+4, columnspan=2, pady=20, padx=20)
 
 botaoFechar = Button(frame_interno,
                      text='Fechar',
                      fg='white',
                      bg='Red',
-                     command=janela.quit,  # Use janela.quit para fechar a janela principal
+                     command=janela.quit,
                      font=("Roboto", 13),
                      relief="ridge",
                      border=2,
